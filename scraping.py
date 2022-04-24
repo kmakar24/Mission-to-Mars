af072daf7fe6cc73ed0c721cb3cc76b510391d98
Kristen Makar <kristenmakar@MacBook-Pro-2.lan>
2022-04-24 18:53:18 -0400

    new code

--- scraping.py
+++ scraping.py
@@ -55,10 +55,7 @@ def mars_news(browser):
         return None, None
 
     return news_title, news_p
-##END OF RETURNING NEWS TITLE & PARAGRAPH
- 
-###JPL Space Images Featyred Image####
-##DISPLAYING FEATURED IMAGE
+
 def featured_image(browser):
     # Visit URL
     url = 'https://spaceimages-mars.com'
@@ -71,44 +68,40 @@ def featured_image(browser):
     # Parse the resulting html with soup
     html = browser.html
     img_soup = soup(html, 'html.parser')
-    #img_soup will print fancybox-image html description
+
 
 
     # Add try/except for error handling
     try:
         # Find the relative image url
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-        #img_url_rel will print 'image/featured/mars3.jpg'
+        
     except AttributeError:
         return None
 
     # Use the base url to create an absolute url
     img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-    #img_url will print 'https://spaceimages-mars.com/image/featured/mars3.jpg'
+    
     return img_url
-#END OF RETURNINNG ONE FEATAURED IMAGE
 
-##MARS FACTS
-#TABLES 1 & 2
+
 def mars_facts():
     # Add try/except for error handling
     try:
         # Use 'read_html' to scrape the facts table into a dataframe
         df = pd.read_html('https://galaxyfacts-mars.com')[0]
-        # dr.head will print table from [0,2] rows 0 - 4
+       
     except BaseException:
         return None
 
     # Assign columns and set index of dataframe
     df.columns=['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
-    #df will print table with formatted descriptions
+    
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
-    ##End of tables
 
-## HEMISPHERES 
 def hemisphere(browser):
 # 1. Use browser to visit the URL 
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
@@ -116,7 +109,7 @@ def hemisphere(browser):
 
 # 2. Create a list to hold the images and titles.
     hemisphere_image_urls = []
-    #imgs_links= browser.find_by_css("a.product-item h3")
+    
 
 # 3. Write code to retrieve the image urls and titles for each hemisphere.
     for i in range(4):      
@@ -131,7 +124,7 @@ def hemisphere(browser):
         hemisphere_image_urls.append(hemispheres)
         browser.back()    
     return hemisphere_image_urls
-##END of SCRAPE ALL
+
 if __name__ == "__main__":
-    # If running as script, print scraped data
+    
     print(scrape_all())
